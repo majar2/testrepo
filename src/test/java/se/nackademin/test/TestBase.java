0295bd6f85a0857af1cfa9b0d9b5e31de01ee2ba
majar2 <martin.jarlefors@cybercom.com>
2019-04-15 10:52:29 +0200

    newedit

--- src/test/java/se/nackademin/test/TestBase.java
+++ src/test/java/se/nackademin/test/TestBase.java
@@ -6,6 +6,7 @@ import static com.codeborne.selenide.Selenide.open;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.openqa.selenium.PageLoadStrategy;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 
@@ -17,7 +18,15 @@ public class TestBase {
     @Before
     public void setUp() {
         ChromeOptions options = new ChromeOptions();
+        options.addArguments("enable-automation");
+        options.addArguments("--headless");
+        options.addArguments("--window-size=1920,1080");
         options.addArguments("--no-sandbox");
+        options.addArguments("--disable-extensions");
+        options.addArguments("--dns-prefetch-disable");
+        options.addArguments("--disable-gpu");
+        options.setPageLoadStrategy(PageLoadStrategy.NORMAL);
+        
         open("https://nackademin.se");
     }
     
